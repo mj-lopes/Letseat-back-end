694eee0d71b9b4fa6c97803af97f8959e0c398ac
mj-lopes <mlrj.junior@gmail.com>
2022-04-12 21:36:32 -0300

    Refatorado nome dos path das rotas

--- src/routes/receitas.route.ts
+++ src/routes/receitas.route.ts
@@ -4,10 +4,10 @@ import rc from "../controller/receita.controller";
 const routerReceitas = Router();
 
 // routerReceitas.get("/save", rc.salvarReceitas);
-routerReceitas.post("/", rc.pegarTodasReceitas);
-routerReceitas.post("/ingredientes", rc.pesquisarPorIngredientes);
-routerReceitas.post("/categoria/:categoria", rc.pesquisarPorCategoria);
-routerReceitas.post("/receitas/:nomeReceita", rc.pesquisarPorNome);
-routerReceitas.post("/receita/:idReceita", rc.pegarReceitasPorId);
+routerReceitas.post("/receitas", rc.pegarTodasReceitas);
+routerReceitas.post("/busca/ingredientes", rc.pesquisarPorIngredientes);
+routerReceitas.post("/busca/:nomeReceita", rc.pesquisarPorNome);
+routerReceitas.post("/receitas/categoria/:categoria", rc.pesquisarPorCategoria);
+routerReceitas.get("/receitas/receita/:idReceita", rc.pegarReceitasPorId);
 
 export default routerReceitas;
